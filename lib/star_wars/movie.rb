7400315b0aa5a03ed335eb7f1635cb0b34dba276
Ryan Lynch <lynchrbusiness@gmail.com>
2019-12-06 07:05:42 +0000

    Created Movie Class

---
+++ lib/star_wars/movie.rb
@@ -0,0 +1,22 @@
+class Movie
+  
+  attr_reader :title, :episode_id, :opening_crawl, :director, :producer, :release_date
+  @@all = []
+  
+  def initialize(title, episode_id, opening_crawl, director, producer, release_date)
+    @title = title
+    @episode_id = episode_id
+    @opening_crawl = opening_crawl
+    @director = director
+    @producer = producer
+    @release_date = release_date
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+end
+  
+  
